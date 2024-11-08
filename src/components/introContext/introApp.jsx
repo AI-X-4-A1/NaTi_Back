c899c56c077af060ac162ee6596b8e838f36449e
ppudding3861 <ppuding3861@gmail.com>
2024-11-08 14:47:10 +0900

    test

--- src/components/introContext/introApp.jsx
+++ src/components/introContext/introApp.jsx
@@ -4,7 +4,7 @@ function IntroApp() {
   return (
     <div>
 
-      <div className="title">형석이 형석이19 소개</div>
+      <div className="title">형석이 형석이20 소개</div>
 
       <div className="context">
         <ul>
